76e050ba92687aeece4d32ca212b752a05ec15b6
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-01 09:01:24 +0530

    Force committed solution - Time: O(n), Space: O(1)

---
+++ 703297-largest-in-array-GFG/703297-largest-in-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int largest(int[] arr) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
