465c3824675a0b38724ea99937d5d13c4e3a33f2
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-09 19:28:38 +0530

    Added solution - Time: O(n*m*log(max - min)), Space: O(1)

--- 704712-median-in-a-row-wise-sorted-matrix-GFG/704712-median-in-a-row-wise-sorted-matrix.java
+++ 704712-median-in-a-row-wise-sorted-matrix-GFG/704712-median-in-a-row-wise-sorted-matrix.java
@@ -1,20 +1,49 @@
-import java.util.*;
-
 class Solution {
-    int median(int[][] mat) {
-        
-        ArrayList<Integer> list = new ArrayList<>();
+    int upperBound(int[] arr, int x) {
+        int low = 0, high = arr.length;
 
-        
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                list.add(mat[i][j]);
-            }
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] <= x)
+                low = mid + 1;
+            else
+                high = mid;
         }
 
-        Collections.sort(list);
-        int mid = list.size() / 2;
-        return list.get(mid);
+        return low;
+    }
+
+    int median(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            low = Math.min(low, mat[i][0]);
+            high = Math.max(high, mat[i][m - 1]);
+        }
+
+        int required = (n * m) / 2;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                count += upperBound(mat[i], mid);
+            }
+
+            if (count <= required)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return low;
     }
 }
 
