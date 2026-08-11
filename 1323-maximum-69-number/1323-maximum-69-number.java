5acb39834a99f2a2eec8e03b80e5d755dbc08aff
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-11 22:37:06 +0530

    Time: 1 ms (77.51%), Space: 42.1 MB (80.94%) - LeetHub - Time: O(n), Space: O(n)

--- 1323-maximum-69-number/1323-maximum-69-number.java
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -1,27 +1,15 @@
 class Solution {
     public int maximum69Number(int num) {
-        String str=String.valueOf(num);
-        char[] arr=str.toCharArray();
-        int l=0;
-        int r=arr.length-1;
+        char[] arr=String.valueOf(num).toCharArray();
 
-        while(l<=r) {
-            if(arr[l]=='9')
-                l++;
-            else {
-                swap(arr,l,l);
-                arr[l]='9';
+        for(int i=0;i<arr.length;i++) {
+            if(arr[i]=='6') {
+                arr[i]='9';
                 break;
             }
         }
 
         return Integer.parseInt(new String(arr));
-    }
-
-    public void swap(char[] arr,int i,int j) {
-        char temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
     }
 }
 
