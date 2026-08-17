c550cf60948b5e5e21633415189f2f40e3c36db5
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-17 19:51:50 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 975381_search-in-a-linked-list.java
@@ -0,0 +1,40 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution{
+    public static int searchInLinkedList(Node head, int k){
+        Node temp=head;
+        while(temp != null){
+            if(temp.data==k) return 1;
+            temp=temp.next;
+        }
+        return 0;
+    }
+}
