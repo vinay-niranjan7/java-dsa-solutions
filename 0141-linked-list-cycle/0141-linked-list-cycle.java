6da9857251bf05ed8feda97deb32705f846b2897
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-23 13:13:42 +0530

    Time: 425 ms (9.66%), Space: 46.6 MB (87.67%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode tmp = head;
+        ArrayList<ListNode> list = new ArrayList<>();
+
+        while (tmp != null) {
+            if (list.contains(tmp)) {
+                return true;
+            }
+            list.add(tmp);
+            tmp = tmp.next;
+        }
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
