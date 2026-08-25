29d1747b933449d1a50dc40bee6167938583117f
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-08-25 11:00:23 +0530

    Force committed solution

---
+++ 700620-cycle-length-in-linked-list-GFG/700620-cycle-length-in-linked-list.java
@@ -0,0 +1,28 @@
+/* Structure of Linked List Node
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}*/
+
+class Solution {
+    public int lengthOfLoop(Node head) {
+        Node tmp=head;
+        ArrayList<Node> list= new ArrayList<>();
+        while(tmp!=null){
+            if(list.contains(tmp)) return list.size()-list.indexOf(tmp);
+            
+            list.add(tmp);
+            tmp=tmp.next;
+        }
+        return 0;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
