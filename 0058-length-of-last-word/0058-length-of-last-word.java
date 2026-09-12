dea669ced493de201b9755061ce3e52664d4a0a1
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-09-12 23:31:33 +0530

    Time: 0 ms (100%), Space: 43 MB (51.15%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' ') count++;
+            if(s.charAt(i)==' ' && count>0) break;
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
