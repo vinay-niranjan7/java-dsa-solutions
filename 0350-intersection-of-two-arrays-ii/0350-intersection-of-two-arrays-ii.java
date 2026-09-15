ef36e5e5e1142f9b3dd0aa2048093cadf2bbbc2b
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-09-15 09:47:05 +0530

    Time: 12 ms (5.1%), Space: 44.6 MB (89.88%) - LeetHub - Time: O(n*m), Space: O(n)

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        ArrayList<Integer> list=new ArrayList<>();
+        ArrayList<Integer> ans=new ArrayList<>();
+
+        for(int i=0;i<nums1.length;i++){
+            list.add(nums1[i]);
+        }
+
+        for(int i=0;i<nums2.length;i++){
+            if(list.contains(nums2[i])){
+                ans.add(nums2[i]);
+                list.remove(Integer.valueOf(nums2[i]));
+            }
+        }
+
+        int[] result=new int[ans.size()];
+        
+        for(int i=0;i<ans.size();i++) {
+            result[i]=ans.get(i);
+        }
+        return result;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
