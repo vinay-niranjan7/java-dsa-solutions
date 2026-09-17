2a65d8e7ce931dcf569e39c59f2cd425379c43e1
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-09-17 19:25:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(k * n), Space: O(1)

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+
+        if(head==null || head.next==null) {
+            return head;
+        }
+
+        int cnt=0;
+        while(cnt<k){
+
+            ListNode tmp=head;
+            ListNode prev=null;
+
+            while(tmp.next!=null){
+                prev=tmp;
+                tmp=tmp.next;
+            }
+
+            tmp.next=head;
+            prev.next=null;
+            head=tmp;
+            cnt++;
+        }
+        return head;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
