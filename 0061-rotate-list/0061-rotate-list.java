774d005d44809bf6ddeea59050cff4e719e00a8e
Vinay Niranjan <vinayniranjan7@gmail.com>
2026-09-17 19:31:26 +0530

    Time: 0 ms (100%), Space: 44.3 MB (62.73%) - LeetHub - Time: O(k * n), Space: O(1)

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
 
-        if(head==null || head.next==null) {
+        if(head==null || head.next==null || k==0) {
             return head;
         }
 
